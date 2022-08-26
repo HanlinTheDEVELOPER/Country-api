5a1ad73d76930ee444c54f478f7e81619dfd68d5
jack <aungkhanthein314@gmail.com>
2022-08-26 12:26:11 +0630

    AungKhantHein_FetchAndDisplayComplete

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const solidMoon = document.getElementById("solid-moon"); // display-none in defa
 
 // DOM Element for displaying fetch data
 const countriesBox = document.getElementById("countries-box");
+const loadingWrapper = document.getElementsByClassName("loadingWrapper")[0];
 
 //  DOM Element for search
 const searchInput = document.getElementById("search-input");
@@ -27,16 +28,32 @@ const oceania = document.getElementById("Oceania");
 const main = document.getElementsByClassName("main")[0];
 const selection = document.getElementsByClassName("selection")[0]; // display none in default
 
-///// Player2 Starts From Here.
+///// Player2 Starts From Here. /////////////////
 // Fetch All Data
 const api = "https://restcountries.com/v3.1/all";
+let a = [];
 fetch(api)
   .then(response => response.json())
   .then(data => getData(data));
 
 // getData with functions from this getData function
 function getData(data) {
-
+  displayCountries(data);
 }
 
-///// Player2 Ends Here.
+// display countries
+function displayCountries(countries) {
+  for(let country of countries) {
+      countriesBox.insertAdjacentHTML("beforeend",
+      `<div class="country">
+        <img src="${country.flags.png}" alt="flag" />
+        <h2>${country.name.common}</h2>
+        <h3>Population: <span>${country.population}</span></h3>
+        <h3>Region: <span>${country.region}</span></h3>
+        <h3>Capital: <span>${country.capital}</span></h3>
+      </div>`)
+  }
+  countriesBox.style.visibility = "visible";
+  loadingWrapper.remove();
+}
+///// Player2 Ends Here. /////////////////
